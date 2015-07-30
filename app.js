3b3152dafe957385bc7b8d2ba2d8d4d56ca94b4c
Roberto Abril <teskostudio@gmail.com>
2015-07-30 14:01:28 +0200

    Editar Preguntas
    Se añadió in formulario de editar preguntas mediante GET
    /quizes/:quizId/edit en el controlador controller/quiz_controller.js así
    como el update mediante /quizes/:quizId
    
    Se añadieron las ruta en routes/index.js (GET/quizes/:quizId/edit y POST
    /quizes/update). Se creo una nueva vista (quizes/edit.ejs) que a su vez
    usa el formulario de creación de preguntas (quizes/_form.ejs) haciendo
    simplemente un method-override de POST a PUT
    
    A fin de usar method-override se instaló el módulo correspondiente
    (2.3.1 ) y se instaló el middleware en app.js
    
    También se modificó la vista index.ejs a fin de añadir un botón que
    cargue edit.ejs pasando el parámetro quizId para editar la pregunta
    correspondiente.
    
    Personalmente modifiqué el alert() en layout.ejs para informar del
    progreso personal del curso a mis posibles correctores del ejercicio P2P
    obligatorio del módulo 7.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials');
+var methodOverride = require('method-override');
 
 var routes = require('./routes/index');
 //var users = require('./routes/users');
@@ -24,6 +25,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // Modulo 8.1 false a true
 app.use(cookieParser());
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -104,3 +104,31 @@ exports.create = function(req, res) {
 			}
 		});
 };
+
+
+exports.edit = function(req, res) {
+
+	var quiz = req.quiz; //autoload de instancia quiz
+	res.render('quizes/edit', {quiz: quiz, errors : [] });
+
+};
+
+exports.update = function(req, res) {
+
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+	
+	req.quiz
+	.validate()
+	.then(
+		function(err) {
+			if (err) {
+				res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+			}else {
+				req.quiz //save: guarda campos pregunta y respuesta en DB
+				.save({ fields : ['pregunta', 'respuesta'] })
+				.then( function() { res.redirect('/quizes');});
+			}
+		}
+	);
+};
